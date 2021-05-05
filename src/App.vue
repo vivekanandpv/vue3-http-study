86d074234d73f3f68f77ed24c21bdbb6704b83f2
vivek <vivek@athenaeum.in>
2021-05-05 17:37:07 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container p-5">
     <h3>Learning HTTP communication in Vue 3</h3>
+    <button class="btn btn-primary" @click="handler">Get</button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue, { createApp } from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import { reqresInstance, typicodeInstance } from './services/http-client';
 
-Vue.use({
-  install(Vue) {
-    Vue.prototype.$reresService = reqresInstance;
-    Vue.prototype.$typicodeService = typicodeInstance;
-  },
-});
+//  injector configuration has changed in Vue 3
+//  Vue.use doesn't work anymore
 
-createApp(App).mount('#app');
+createApp(App)
+  .use({
+    install(Vue) {
+      console.log(Vue);
+      Vue.config.globalProperties.$reqresService = reqresInstance;
+    },
+  })
+  .use({
+    install(Vue) {
+      console.log(Vue);
+      Vue.config.globalProperties.$typicodeService = typicodeInstance;
+    },
+  })
+  .mount('#app');
